0a96a64754bdcabd7b0275a306c1f81c970ada8b
Dariga Iskakova <darigaiskakova@icloud.com>
2022-04-30 17:20:56 -0400

    api calls work

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,6 +16,35 @@ app.get("/notes", (req, res) => {
 })
 
 //API
+app.get("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
+        if(err) throw err;
+
+        const notes = JSON.parse(data)
+
+        res.json(notes)
+    })
+})
+
+app.post("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
+        if(err) throw err;
+
+        const notes = JSON.parse(data);
+        const newNote = req.body;
+
+        const newID = Math.floor(Math.random() * 60000000000);
+        newNote.id = newID
+        
+        notes.push(newNote)
+
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notes), (err) => {
+            if(err) throw err;
+
+            res.json("added a new note")
+        })
+    })
+})
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
