934152e2b683f57d999acec23a744980e3e1cedb
Dariga Iskakova <darigaiskakova@icloud.com>
2022-04-30 17:12:45 -0400

    finished HTML routes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,6 +9,16 @@ app.use(express.json())
 app.use(express.static("public"))
 
 //routes
+//HTML
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
+//API
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+})
 
 app.listen(PORT, () => {
     console.log("alive")
